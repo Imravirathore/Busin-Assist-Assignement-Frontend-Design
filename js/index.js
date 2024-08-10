c120c66c04fd4b8e5df8fe05ea65f7421c5516be
Imravirathore <ravirathore672@gmail.com>
2024-08-10 19:11:43 +0530

    initial files

---
+++ js/index.js
@@ -0,0 +1,4 @@
+        window.addEventListener('load', function() {
+            document.getElementById('loader').style.display = 'none';
+            document.querySelector('.content').style.display = 'block';
+          });
